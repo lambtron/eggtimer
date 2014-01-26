484bbe989bbc92ab4d85cb0363f11709a8b67554
Andy Jiang <andyjiang@gmail.com>
2014-01-26 00:01:31 -0800

    now posts to server the user data

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -7,7 +7,55 @@ eggTimer.controller('mainController', ['$scope', '$http',
     first_name: '',
     email_address: '',
     zip_code: '',
-    reminders: []
+    reminders: [
+    	{
+    		reminder: 'General health check up',
+    		start_date: '01/01/14',
+    		frequency: 'Annually'
+    	},
+    	{
+    		reminder: 'Dentist cleaning',
+    		start_date: '01/01/14',
+    		frequency: 'Biannually'
+    	},
+    	{
+    		reminder: 'Rent',
+    		start_date: '01/01/14',
+    		frequency: 'Monthly'
+    	}
+    ]
+  };
+
+  var frequencies = $scope.frequencies = [
+  	'Annually',
+  	'Biannually',
+  	'Quarterly',
+  	'Monthly',
+  	'Weekly',
+  	'Daily'
+  ];
+
+  var addReminder = $scope.addReminder = function() {
+  	var obj = {};
+  	obj.reminder = 'New reminder';
+  	obj.start_date = '01/01/14';
+  	obj.frequency = 'Annually';
+  	user.reminders.push(obj);
+  };
+
+  var removeReminder = $scope.removeReminder = function(index) {
+  	user.reminders.splice(index,1);
+  };
+
+  var submit = $scope.submit = function() {
+  	// Submit user to server.
+  	$http.post('/api/user', user)
+		.success(function(data) {
+			console.log('Success: ' + data);
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
   };
 
 }]); 
