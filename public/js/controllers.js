b29461b2c5d86e02bb94cc50f4162e7a8d1576ab
Andy Jiang <andyjiang@gmail.com>
2014-01-25 22:44:25 -0800

    added more stuff to front end

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,6 +3,11 @@
 eggTimer.controller('mainController', ['$scope', '$http', 
 	function($scope, $http)
 {
-
+  var user = $scope.user = {
+    first_name: '',
+    email_address: '',
+    zip_code: '',
+    reminders: []
+  };
 
 }]); 
--- public/js/index.js
+++ public/js/index.js
@@ -1,48 +1,11 @@
 'use strict';
 
-$(document).ready(function() {
-	var fileInput = document.getElementById('address-file-upload');
-	var csvFile = {};
-
-	fileInput.addEventListener('change', function(e) {
-		var file = fileInput.files[0];
-		var textType = /text.*/;
-
-		// Use DOM to get AngularJS root scope.
-		var scope = angular.element(this).scope();
-
-		if (file.type.match(textType)) {
-			var reader = new FileReader();
-
-			reader.onload = function(e) {
-				var csvString = reader.result;
-				console.log(csvString);
-				
-				// Clean the headers.
-				var firstLine = csvString.split('\n')[0];
-				var cleanedFirstLine = firstLine.toLowerCase().replace(/\s+/g, '');
-				csvString = csvString.replace(firstLine, cleanedFirstLine);
-
-				// Parse CSV.
-				csvFile = $.csv.toObjects(csvString);
-
-				scope.$apply(function() {
-					for(var i = 0; i < csvFile.length; i++ ) {
-						// First getAddressId of each item, which will then update the model.
-						csvFile[i].type = 'to';
-						scope.getAddressId(csvFile[i]);
-						// scope.toAddresses.addresses.push(csvFile[i]);
-					};
-				});
-			};
-
-			reader.readAsText(file);
-		} else {
-			// Error.
-
-		};
+$(document).ready(function() {	
+	$(".dropdown-menu li a").click(function(){
+		console.log(this);
+		var a = $(this).closest("form-group").find(".selection");
+		console.log(a);
+	  $(this).closest(".form-group").find(".selection").text($(this).text());
+	  $(this).closest(".form-group").find(".selection").val($(this).text());
 	});
-
-	// Setup stripe.
-  Stripe.setPublishableKey('pk_live_zOfOwq7NI2ajzRKakRJa54mZ');
 });
